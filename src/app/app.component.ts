0816f4f295e7b8972b6b7b8ba52ecbe79465ef78
sonik91 <pierre.gonet91@gmail.com>
2021-02-19 13:11:55 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Loader } from "@googlemaps/js-api-loader";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  loader = new Loader({
+    apiKey: "YOUR_API_KY",
+    version: "weekly"
+  });
+
+  ngOnInit() {}
+
+  initMap() {
+    this.loader.load().then(() => {
+      let map = new google.maps.Map(
+        document.getElementById("map") as HTMLElement,
+        {
+          center: { lat: -34.397, lng: 150.644 },
+          zoom: 8
+        }
+      );
+    });
+  }
 }
